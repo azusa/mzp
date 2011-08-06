98b1b0de0d07aa613b801fdda845692cc542668a
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2011-08-06 23:36:49 +0900

    [from now] 2011年 8月 6日 土曜日 23時36分49秒 JST
    diff --git a/src/main/java/jp/fieldnotes/mzp/Timeline.java b/src/main/java/jp/fieldnotes/mzp/Timeline.java
    index 9c05067..9d40d9d 100644
    --- a/src/main/java/jp/fieldnotes/mzp/Timeline.java
    +++ b/src/main/java/jp/fieldnotes/mzp/Timeline.java
    @@ -1,16 +1,44 @@
     package jp.fieldnotes.mzp;
    
    +import java.io.BufferedReader;
    +import java.io.IOException;
    +import java.io.InputStreamReader;
     import java.net.URL;
    +import java.net.URLConnection;
    +import java.util.ArrayList;
     import java.util.List;
    
     public class Timeline {
    
    +	private final List<Tweet> tweetList = new ArrayList<Tweet>();
    +
     	public Timeline(URL url) {
    -		// TODO Auto-generated constructor stub
    +		URLConnection connection = null;
    +		BufferedReader br = null;
    +		try {
    +			connection = url.openConnection();
    +			br = new BufferedReader(new InputStreamReader(
    +					connection.getInputStream()));
    +			String line = br.readLine();
    +			while (line != null) {
    +				tweetList.add(new Tweet(line));
    +				line = br.readLine();
    +			}
    +		} catch (IOException e) {
    +			throw new RuntimeException(e);
    +		} finally {
    +			if (br != null) {
    +				try {
    +					br.close();
    +				} catch (IOException e) {
    +				}
    +			}
    +
    +		}
     	}
    
     	public List<Tweet> getTweetList() {
    -		return null;
    +		return tweetList;
     	}
    
     }

--- src/main/java/jp/fieldnotes/mzp/Timeline.java
+++ src/main/java/jp/fieldnotes/mzp/Timeline.java
@@ -1,16 +1,44 @@
 package jp.fieldnotes.mzp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Timeline {
 
+	private final List<Tweet> tweetList = new ArrayList<Tweet>();
+
 	public Timeline(URL url) {
-		// TODO Auto-generated constructor stub
+		URLConnection connection = null;
+		BufferedReader br = null;
+		try {
+			connection = url.openConnection();
+			br = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
+			String line = br.readLine();
+			while (line != null) {
+				tweetList.add(new Tweet(line));
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+				}
+			}
+
+		}
 	}
 
 	public List<Tweet> getTweetList() {
-		return null;
+		return tweetList;
 	}
 
 }
