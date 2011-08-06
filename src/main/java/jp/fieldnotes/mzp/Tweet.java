d3372b9f78b39cc4767280e210ca5ccefb5a50f7
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2011-08-06 23:02:54 +0900

    [from now] 2011年 8月 6日 土曜日 23時02分54秒 JST
    diff --git a/src/main/java/jp/fieldnotes/mzp/Tweet.java b/src/main/java/jp/fieldnotes/mzp/Tweet.java
    index 43f905a..ff0f70c 100644
    --- a/src/main/java/jp/fieldnotes/mzp/Tweet.java
    +++ b/src/main/java/jp/fieldnotes/mzp/Tweet.java
    @@ -1,5 +1,8 @@
     package jp.fieldnotes.mzp;
    
    +import java.util.ArrayList;
    +import java.util.List;
    +
     public class Tweet {
    
     	private final String tweet;
    @@ -11,14 +14,17 @@ public class Tweet {
     	public String getTweet() {
     		String[] elements = this.tweet.split("\t");
     		String body = elements[elements.length - 1];
    +		List<String> judge = new ArrayList<String>();
     		if (body.startsWith("@")) {
    -			return "Reply\t" + body;
    +			judge.add("Reply");
     		} else if (body.contains("@")) {
    -			return "Mention\t" + body;
    +			judge.add("Mention");
     		} else if (body.contains("#")) {
    -			return "!HashTag\t" + body;
    -		} else {
    +			judge.add("!HashTag");
    +		}
    +		if (judge.isEmpty()) {
     			return "Normal\t" + body;
     		}
    +		return null;
     	}
     }

--- src/main/java/jp/fieldnotes/mzp/Tweet.java
+++ src/main/java/jp/fieldnotes/mzp/Tweet.java
@@ -1,5 +1,8 @@
 package jp.fieldnotes.mzp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tweet {
 
 	private final String tweet;
@@ -11,14 +14,17 @@ public class Tweet {
 	public String getTweet() {
 		String[] elements = this.tweet.split("\t");
 		String body = elements[elements.length - 1];
+		List<String> judge = new ArrayList<String>();
 		if (body.startsWith("@")) {
-			return "Reply\t" + body;
+			judge.add("Reply");
 		} else if (body.contains("@")) {
-			return "Mention\t" + body;
+			judge.add("Mention");
 		} else if (body.contains("#")) {
-			return "!HashTag\t" + body;
-		} else {
+			judge.add("!HashTag");
+		}
+		if (judge.isEmpty()) {
 			return "Normal\t" + body;
 		}
+		return null;
 	}
 }
