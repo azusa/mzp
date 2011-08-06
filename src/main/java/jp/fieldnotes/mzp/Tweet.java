db908d88218963fef13ffb956745518b2ba6fe65
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2011-08-06 15:38:32 +0900

    [from now] 2011年 8月 6日 土曜日 15時38分32秒 JST
    diff --git a/src/main/java/jp/fieldnotes/mzp/Tweet.java b/src/main/java/jp/fieldnotes/mzp/Tweet.java
    index 8ff94c4..4621d05 100644
    --- a/src/main/java/jp/fieldnotes/mzp/Tweet.java
    +++ b/src/main/java/jp/fieldnotes/mzp/Tweet.java
    @@ -2,4 +2,13 @@ package jp.fieldnotes.mzp;
    
     public class Tweet {
    
    +	public Tweet(String string) {
    +		// TODO Auto-generated constructor stub
    +	}
    +
    +	public String getTweet() {
    +		// TODO Auto-generated method stub
    +		return null;
    +	}
    +
     }

--- src/main/java/jp/fieldnotes/mzp/Tweet.java
+++ src/main/java/jp/fieldnotes/mzp/Tweet.java
@@ -2,4 +2,13 @@ package jp.fieldnotes.mzp;
 
 public class Tweet {
 
+	public Tweet(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getTweet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
