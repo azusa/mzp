f38143a5ca42a65baa55bda9d4de05da071920ca
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2011-08-06 23:27:27 +0900

    [from now] 2011年 8月 6日 土曜日 23時27分27秒 JST
    diff --git a/src/test/java/jp/fieldnotes/mzp/TimelineTest.java b/src/test/java/jp/fieldnotes/mzp/TimelineTest.java
    index ac33762..0a9b068 100644
    --- a/src/test/java/jp/fieldnotes/mzp/TimelineTest.java
    +++ b/src/test/java/jp/fieldnotes/mzp/TimelineTest.java
    @@ -1,7 +1,10 @@
     package jp.fieldnotes.mzp;
    
    +import static org.hamcrest.CoreMatchers.*;
     import static org.junit.Assert.*;
    
    +import java.net.URL;
    +
     import org.junit.Test;
    
     public class TimelineTest {
    @@ -9,7 +12,7 @@ public class TimelineTest {
     	@Test
     	public void ネットワーク越しの取得ができること() {
    
    +		Timeline timeline = new Timeline(new URL("http://tddbc.heroku.com/mzp/public_timeline"))
     		assertThat(timeline.getTweetList().size, is(20));
     	}
    -
     }

--- src/test/java/jp/fieldnotes/mzp/TimelineTest.java
+++ src/test/java/jp/fieldnotes/mzp/TimelineTest.java
@@ -1,6 +1,9 @@
 package jp.fieldnotes.mzp;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+
+import java.net.URL;
 
 import org.junit.Test;
 
@@ -9,7 +12,7 @@ public class TimelineTest {
 	@Test
 	public void ネットワーク越しの取得ができること() {
 
+		Timeline timeline = new Timeline(new URL("http://tddbc.heroku.com/mzp/public_timeline"))
 		assertThat(timeline.getTweetList().size, is(20));
 	}
-
 }
