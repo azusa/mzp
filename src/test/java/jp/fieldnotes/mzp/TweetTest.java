504f30a7f59fb0ef3659ab697380d12ba2df9925
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2011-08-06 15:36:55 +0900

    [from now] 2011年 8月 6日 土曜日 15時36分55秒 JST
    diff --git a/src/test/java/jp/fieldnotes/mzp/TweetTest.java b/src/test/java/jp/fieldnotes/mzp/TweetTest.java
    index 60a2a00..125055b 100644
    --- a/src/test/java/jp/fieldnotes/mzp/TweetTest.java
    +++ b/src/test/java/jp/fieldnotes/mzp/TweetTest.java
    @@ -1,5 +1,6 @@
     package jp.fieldnotes.mzp;
    
    +import static org.hamcrest.CoreMatchers.*;
     import static org.junit.Assert.*;
    
     import org.junit.Ignore;
    @@ -8,7 +9,8 @@ import org.junit.Test;
     public class TweetTest {
     	@Test
     	public void replayでもmentionでもhashtagでもないtweetを普通のtweetと判定する() {
    -		fail();
    +
    +		assertThat(tweet.getNormal(), is("Normal\tあいうえお"));
     	}
    
     	@Test

--- src/test/java/jp/fieldnotes/mzp/TweetTest.java
+++ src/test/java/jp/fieldnotes/mzp/TweetTest.java
@@ -1,5 +1,6 @@
 package jp.fieldnotes.mzp;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.Ignore;
@@ -8,7 +9,8 @@ import org.junit.Test;
 public class TweetTest {
 	@Test
 	public void replayでもmentionでもhashtagでもないtweetを普通のtweetと判定する() {
-		fail();
+
+		assertThat(tweet.getNormal(), is("Normal\tあいうえお"));
 	}
 
 	@Test
