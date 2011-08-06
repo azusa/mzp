4d8e37b1254ec53c19c428195ebfa6e547f63db2
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2011-08-06 15:34:18 +0900

    [from now] 2011年 8月 6日 土曜日 15時34分18秒 JST
    diff --git a/src/main/java/jp/fieldnotes/mzp/Tweet.java b/src/main/java/jp/fieldnotes/mzp/Tweet.java
    new file mode 100644
    index 0000000..8ff94c4
    --- /dev/null
    +++ b/src/main/java/jp/fieldnotes/mzp/Tweet.java
    @@ -0,0 +1,5 @@
    +package jp.fieldnotes.mzp;
    +
    +public class Tweet {
    +
    +}
    diff --git a/src/test/java/jp/fieldnotes/mzp/TweetTest.java b/src/test/java/jp/fieldnotes/mzp/TweetTest.java
    new file mode 100644
    index 0000000..be1e7d1
    --- /dev/null
    +++ b/src/test/java/jp/fieldnotes/mzp/TweetTest.java
    @@ -0,0 +1,14 @@
    +package jp.fieldnotes.mzp;
    +
    +import static org.junit.Assert.*;
    +
    +import org.junit.Test;
    +
    +public class TweetTest {
    +
    +	@Test
    +	public void test() {
    +		fail("Not yet implemented");
    +	}
    +
    +}

---
+++ src/main/java/jp/fieldnotes/mzp/Tweet.java
@@ -0,0 +1,5 @@
+package jp.fieldnotes.mzp;
+
+public class Tweet {
+
+}
---
+++ src/test/java/jp/fieldnotes/mzp/TweetTest.java
@@ -0,0 +1,14 @@
+package jp.fieldnotes.mzp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TweetTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
